e057f51e4599b5d25e47421da220d1a1fdee1d34
Lexi <alkeller@inprs.in.gov>
2025-06-17 09:56:52 -0400

    nav bar

--- js/components/navigation.js
+++ js/components/navigation.js
@@ -1,34 +1,72 @@
-// SIDE NAV --- This changes the content on the page depending on the tab
+// // SIDE NAV --- This changes the content on the page depending on the tab
+// var navItems = document.querySelectorAll('.nav-item');
+// var contentSections = document.querySelectorAll('.content-section');
+
+// function showContent(tabId) {
+//     // Hide all content sections
+//     for (var i = 0; i < contentSections.length; i++) {
+//         contentSections[i].classList.add('hidden');
+//     }
+
+//     // Show selected content section
+//     var targetSection = document.getElementById(tabId);
+//     if (targetSection) {
+//         targetSection.classList.remove('hidden');
+//     }
+
+//     // Update active states
+//     for (var j = 0; j < navItems.length; j++) {
+//         navItems[j].classList.remove('active');
+//     }
+
+//     // Add active class to clicked item
+//     var activeItem = document.querySelector('[data-tab="' + tabId + '"]');
+//     if (activeItem) {
+//         activeItem.classList.add('active');
+//     }
+// }
+
+
+
+// // Initialize with first tab active
+// showContent('components-buttons');
+
 var navItems = document.querySelectorAll('.nav-item');
-var contentSections = document.querySelectorAll('.content-section');
+        var contentSections = document.querySelectorAll('.content-section');
 
-function showContent(tabId) {
-    // Hide all content sections
-    contentSections.forEach(function(section) {
-        section.classList.add('hidden');
-    });
+        function showContent(tabId) {
+            // Hide all content sections
+            for (var i = 0; i < contentSections.length; i++) {
+                contentSections[i].classList.add('hidden');
+            }
 
-    // Show selected content section
-    var targetSection = document.getElementById(tabId);
-    if (targetSection) {
-        targetSection.classList.remove('hidden');
-    }
+            // Show selected content section
+            var targetSection = document.getElementById(tabId);
+            if (targetSection) {
+                targetSection.classList.remove('hidden');
+            }
 
-    // Update active states - remove from all
-    navItems.forEach(function(item) {
-        item.classList.remove('active');
-    });
+            // Update active states
+            for (var j = 0; j < navItems.length; j++) {
+                navItems[j].classList.remove('active');
+            }
 
-    // Add active class to clicked item
-    var activeItem = document.querySelector('[data-tab="' + tabId + '"]');
-    if (activeItem) {
-        activeItem.classList.add('active');
-    }
-}
+            // Add active class to clicked item
+            var activeItem = document.querySelector('[data-tab="' + tabId + '"]');
+            if (activeItem) {
+                activeItem.classList.add('active');
+            }
+        }
 
+        // Add click event listeners to nav items
+        for (var k = 0; k < navItems.length; k++) {
+            navItems[k].addEventListener('click', function() {
+                var tabId = this.getAttribute('data-tab');
+                showContent(tabId);
+            });
+        }
 
-
-// Initialize with first tab active
-showContent('components-buttons');
+        // Initialize with first tab active
+        showContent('components-buttons');
 
 
